9be988aeb25f075f9bdbf2cd67e7b8c2a41a868b
Maxime Gaudron <maxime.gaudron@rocket-internet.de>
2014-10-30 09:12:13 +0100

    Jira reporting web view

---
+++ src/Rocket/JiraCsBundle/Controller/ReportingController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Rocket\JiraCsBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * Reporting controller.
+ *
+ * @Route("/jira/reporting")
+ */
+class ReportingController extends Controller
+{
+    
+    /**
+     * Worklog reporting.
+     *
+     * @Route("/worklogs", name="jira_project")
+     * @Method("GET")
+     * @Template()
+     */
+    public function worklogsAction()
+    {
+        $weeks = $this->get('rocket_jira_cs.issue_repository')->getTimeReportForTheCurrentWeek();
+        
+        return array(
+            'weeks' => array_reverse($weeks->toArray())
+        );
+    }
+}
